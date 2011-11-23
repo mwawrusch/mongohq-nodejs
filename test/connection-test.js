15f10641c30d2c85b45adf8870ec2263fb9d8f30
coderoshi <eric.redmond@gmail.com>
2011-11-23 14:24:18 -0800

    more vows

--- test/connection-test.js
+++ test/connection-test.js
@@ -1,8 +1,20 @@
 var vows      = require('vows'),
     assert    = require('assert'),
-    vanguard  = require('../lib/vanguard');
+    vanguard  = require('../lib/vanguard'),
+    base      = require('../lib/vanguard/base'),
+    connection = require('../lib/vanguard/connection');
 
 vows.describe('Connections').addBatch({
+  'when not authenticated': {
+    topic: base,
+    'cannot create a connection': {
+      'is not null': function (topic) {
+        assert.throws( function() {topic.connection()}, 'apikey must be set' )
+      },
+    }
+  }
+
+}).addBatch({
   'when authenticating': {
     topic: function () {
       return vanguard.authenticate({'apikey' : 'derp'});
@@ -17,6 +29,40 @@ vows.describe('Connections').addBatch({
       }
     }
   }
+}).addBatch({
 
-  // TODO: connection can call "call"
+  'when making a call': {
+    topic: function () {
+      vanguard.authenticate({'apikey' : 'derp'});
+      vanguard.conn = {'request' : function(){console.log('called')}}
+      return base.connection();
+    },
+
+    'accepts only path': function (topic) {
+      assert.doesNotThrow( function() {topic.call('/')}, Error )
+    },
+
+    'accepts only path and method': function (topic) {
+      assert.doesNotThrow( function() {topic.call('/', 'GET')}, Error )
+    },
+
+    'accepts only path, method, and query': function (topic) {
+      assert.doesNotThrow( function() {topic.call('/', 'GET', {'herp' : 'derp'})}, Error )
+    }
+  },
+
+  'when making a callback': {
+    topic: function () {
+      vanguard.authenticate({'apikey' : 'derp'});
+      vanguard.conn = {'request' : function(opts, callback){ assert(false); }}
+      return base.connection();
+    },
+
+    'accepts callback': function (topic) {
+      topic.call('/', 'GET', {}, function(){
+        assert(true)
+      })
+    }
+  }
 }).export(module);
+
